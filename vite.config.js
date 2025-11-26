0dce9e2df6c129466e8706214a4e3d367b7a6555
aaajiao <aaajiao@gmail.com>
2025-11-26 18:10:00 +0100

    refactor: Update Vite configuration import style and regenerate package lock.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import path from 'path';
-import vite from 'vite';
-const { defineConfig, loadEnv } = vite;
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
